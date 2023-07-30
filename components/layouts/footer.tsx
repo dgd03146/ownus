f67ffb2b66cb3606940cd0df29a796c409841ea3
dgd03146 <ibory1220@naver.com>
2023-07-30 14:24:37 +0100

    design: Make footer responsive #27

--- components/layouts/footer.tsx
+++ components/layouts/footer.tsx
@@ -1,38 +1,46 @@
 import React from 'react';
-import tw from 'twin.macro';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+import Link from 'next/link';
+import {} from 'twin.macro';
 
 const Footer = () => {
   // FIXME: 맵으로 묶어서 정렬하기, 공통 컴포넌트 분리
   return (
     <div tw="py-16 bg-primary1 px-[60px]">
-      <div tw="flex justify-center gap-x-40 mx-auto text-primary3">
+      <div tw="flex justify-center flex-col laptop:flex-row gap-y-4 laptop:gap-x-10 desktop:gap-x-40 mx-auto text-primary3">
         <div>
           <h3 tw="font-Cinzel font-semibold mb-2">OWNUS</h3>
-          <p>© 2020 Qode Interactive, All Rights Reserved</p>
+          <p>©2023 GeoJungIm All Rights Reserved</p>
         </div>
-        <div tw="grid grid-cols-3 gap-x-32">
+        <div tw=" gap-x-20 gap-y-4 desktop:gap-x-32 flex flex-col tablet:grid tablet:grid-cols-3">
           <div>
             <p tw="font-bold mb-4">About</p>
             <ul>
-              <li>About Us</li>
-              <li>Shop</li>
-              <li>Blog</li>
+              <li>Company. Ownus</li>
+              <li>Ltd Owner: Geojungim</li>
+              <li>Address. London, UK</li>
             </ul>
           </div>
           <div>
             <p tw="font-bold mb-4">Contact</p>
             <ul>
-              <li>About Us</li>
-              <li>Shop</li>
-              <li>Blog</li>
+              <li>ibory1220@gmail.com</li>
+              <li>44-1234-5678</li>
             </ul>
           </div>
           <div>
             <p tw="font-bold mb-4">Follow Us</p>
-            <ul>
-              <li>About Us</li>
-              <li>Shop</li>
-              <li>Blog</li>
+            <ul tw="text-xl flex flex-col gap-y-2">
+              <li>
+                <Link href={'https://www.linkedin.com/in/dgd03146'}>
+                  <AiFillLinkedin />
+                </Link>
+              </li>
+              <li>
+                <Link href={'https://github.com/dgd03146'}>
+                  <AiFillGithub />
+                </Link>
+              </li>
             </ul>
           </div>
         </div>
